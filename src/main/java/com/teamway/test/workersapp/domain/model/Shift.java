2425e9841a2caba6a378bac3961fcb0194a5a8d7
tarikmuminovic <muminovic.tarik@nsoft.com>
2022-01-27 00:56:37 +0100

    Define Shift model

---
+++ src/main/java/com/teamway/test/workersapp/domain/model/Shift.java
@@ -0,0 +1,35 @@
+package com.teamway.test.workersapp.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.Getter;
+
+import java.time.LocalTime;
+
+@Getter
+public class Shift {
+
+	private LocalTime start;
+
+	private LocalTime end;
+
+	private ShiftType type;
+
+	@JsonCreator
+	public Shift(ShiftType type) {
+		this.type = type;
+		switch (type) {
+			case ZERO_TO_EIGHT -> {
+				this.start = LocalTime.of(0, 0, 0);
+				this.end = LocalTime.of(8, 0, 0);
+			}
+			case EIGHT_TO_SIXTEEN -> {
+				this.start = LocalTime.of(8, 0, 0);
+				this.end = LocalTime.of(16, 0, 0);
+			}
+			case SIXTEEN_TO_TWENTY_FOUR -> {
+				this.start = LocalTime.of(16, 0, 0);
+				this.end = LocalTime.of(24, 0, 0);
+			}
+		}
+	}
+}
---
+++ src/main/java/com/teamway/test/workersapp/domain/model/ShiftType.java
@@ -0,0 +1,16 @@
+package com.teamway.test.workersapp.domain.model;
+
+public enum ShiftType {
+	/*
+	 * 0h - 8h
+	 */
+	ZERO_TO_EIGHT,
+	/*
+	 * 8h- 16h
+	 */
+	EIGHT_TO_SIXTEEN,
+	/*
+	 * 16h - 24h
+	 */
+	SIXTEEN_TO_TWENTY_FOUR,
+}
