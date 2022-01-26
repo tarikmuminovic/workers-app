ed1890fb136228fca0ecaa1810ba8c99e1b9217d
tarikmuminovic <muminovic.tarik@nsoft.com>
2022-01-27 00:50:27 +0100

    Initial commit
    Commit initial files & add app dependencies

---
+++ src/main/java/com/teamway/test/workersapp/WorkersAppApplication.java
@@ -0,0 +1,13 @@
+package com.teamway.test.workersapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkersAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkersAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamway/test/workersapp/StartApplicationTest.java
@@ -0,0 +1,13 @@
+package com.teamway.test.workersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
